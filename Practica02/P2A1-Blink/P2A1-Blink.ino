ea7b966a0d6b679ed7dad6bd520e1a8d4607d4ee
Cracop <rodrigoalejandrobarrera@gmail.com>
2022-08-19 14:48:05 -0500

    Se prendió el primer led

---
+++ Practica02/P2A1-Blink/P2A1-Blink.ino
@@ -0,0 +1,13 @@
+#define LED 13
+
+void setup() {
+  // put your setup code here, to run once:
+  
+  pinMode(LED, OUTPUT); //PIN(LED, SALIDA O ENTRADA)
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED,HIGH); //HIGH Estado alto lógico 5V, LOW - Estado bajo 0V
+}
