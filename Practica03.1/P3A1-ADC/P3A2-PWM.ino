d046db08558ac33a93d28683b899ec858a01a614
AccelR8 <55751739+Axelord100@users.noreply.github.com>
2022-08-29 12:44:41 -0500

    Add files via upload

---
+++ Practica03.1/P3A1-ADC/P3A2-PWM.ino
@@ -0,0 +1,24 @@
+#define pot A0
+#define led 13
+
+float value = 0;
+float brillo;
+
+int pwm = 0;
+
+void setup() {
+  pinMode(led, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  value = analogRead(pot);
+  pwm = value/4;
+  
+  analogWrite(led, pwm);
+  
+  brillo = value/1023*100;
+  Serial.print("Brillo: ");
+  Serial.println(brillo);
+  delay(1);
+}
